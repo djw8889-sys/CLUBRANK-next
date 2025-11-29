af1ddb76aa18e14772ca3c8cd2eaa247ee4eeeba
djw8889-sys <djw8889@gmail.com>
2025-11-29 14:29:00 +0900

    Fix: TS path alias for server modules

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,35 +1,36 @@
+// src/lib/server/db.ts (전체교체)
+
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-import * as schema from "@db/schema";
+import * as dbSchema from "@db/schema";
 
 const connectionString = process.env.DATABASE_URL;
 
-if (!connectionString) {
-  console.warn("[DB] DATABASE_URL 환경변수가 설정되지 않았습니다. Drizzle 연결이 비활성화됩니다.");
-}
-
-const pool = connectionString
-  ? new Pool({ connectionString })
-  : undefined;
-
-type DrizzleDb = ReturnType<typeof drizzle>;
+let dbInstance: ReturnType<typeof drizzle> | undefined;
 
 declare global {
   // eslint-disable-next-line no-var
-  var __drizzleDb: DrizzleDb | undefined;
+  var __drizzleDb: ReturnType<typeof drizzle> | undefined;
 }
 
-let dbInstance: DrizzleDb;
-
-if (pool) {
+if (process.env.NODE_ENV === "production") {
+  if (connectionString) {
+    const client = new Pool({ connectionString });
+    dbInstance = drizzle(client, { schema: dbSchema });
+  } else {
+    dbInstance = undefined;
+  }
+} else {
   if (!global.__drizzleDb) {
-    global.__drizzleDb = drizzle(pool, { schema });
+    if (connectionString) {
+      const client = new Pool({ connectionString });
+      global.__drizzleDb = drizzle(client, { schema: dbSchema });
+    } else {
+      global.__drizzleDb = undefined;
+    }
   }
   dbInstance = global.__drizzleDb;
-} else {
-  // @ts-expect-error - 런타임에서 사용시 반드시 DATABASE_URL을 세팅해야 합니다.
-  dbInstance = undefined;
 }
 
 export const db = dbInstance;
-export * as dbSchema from "@db/schema";
+export { dbSchema };
